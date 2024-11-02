160885b878f7fec4319f46da7d2a1ab0e2fa57d1
路静 <lujing@lujings-MacBook-Pro.local>
2024-11-02 17:00:47 +0100

    fix bin

--- technique_for.cpp
+++ technique_for.cpp
@@ -13,22 +13,48 @@ void compress_for(const std::string& dataType, const std::string& inputFilePath,
         throw std::runtime_error("Cannot open output file: " + outputFilePath);
     }
 
-    if (dataType == "int8") {
+    // adapt dataType, automatically select a smaller integer type for compression
+    int64_t value;
+    int64_t minValue = std::numeric_limits<int8_t>::max();
+    int64_t maxValue = std::numeric_limits<int8_t>::min();
+
+    while (inputFile >> value) {
+        if (value < minValue) minValue = value;
+        if (value > maxValue) maxValue = value;
+    }
+
+    
+    std::string dataType_adapted;
+    if (minValue >= std::numeric_limits<int8_t>::min() && maxValue <= std::numeric_limits<int8_t>::max()) {
+        dataType_adapted = "int8";
+    } else if (minValue >= std::numeric_limits<int16_t>::min() && maxValue <= std::numeric_limits<int16_t>::max()) {
+        dataType_adapted = "int16_t";
+    } else if (minValue >= std::numeric_limits<int32_t>::min() && maxValue <= std::numeric_limits<int32_t>::max()) {
+        dataType_adapted = "int32_t";
+    } else if (minValue >= std::numeric_limits<int64_t>::min() && maxValue <= std::numeric_limits<int64_t>::max()) {
+        dataType_adapted = "int64_t";
+    }
+
+    inputFile.clear();
+    inputFile.seekg(0, std::ios::beg);
+
+
+    if (dataType_adapted == "int8") {
         int8_t value;
         while (inputFile >> value) {
             outputFile.write(reinterpret_cast<char*>(&value), sizeof(value));
         }
-    } else if (dataType == "int16") {
+    } else if (dataType_adapted == "int16") {
         int16_t value;
         while (inputFile >> value) {
             outputFile.write(reinterpret_cast<char*>(&value), sizeof(value));
         }
-    } else if (dataType == "int32") {
+    } else if (dataType_adapted == "int32") {
         int32_t value;
         while (inputFile >> value) {
             outputFile.write(reinterpret_cast<char*>(&value), sizeof(value));
         }
-    } else if (dataType == "int64") {
+    } else if (dataType_adapted == "int64") {
         int64_t value;
         while (inputFile >> value) {
             outputFile.write(reinterpret_cast<char*>(&value), sizeof(value));
